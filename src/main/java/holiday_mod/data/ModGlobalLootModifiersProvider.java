c4c17020d7c1a077e6220d415f190662b4bce2eb
Anthony Michalek <ianthisawesomee@gmail.com>
2025-05-23 02:34:00 -0400

    License change! (And Broken Village, oops)

--- src/main/java/holiday_mod/data/ModGlobalLootModifiersProvider.java
+++ src/main/java/holiday_mod/data/ModGlobalLootModifiersProvider.java
@@ -1,3 +1,23 @@
+/**
+ *         Minecraft Holiday Mod (WIP description)<br>
+ *         Copyright (C) 2025  Minecraft Holiday Mod Team (WIP name)<br>
+ *         <br>
+ *         This file part of Minecraft Holiday Mod.<br>
+ *         <br>
+ *         This program is free software: you can redistribute it and/or modify<br>
+ *         it under the terms of the GNU General Public License as published by<br>
+ *         the Free Software Foundation, either version 3 of the License, or<br>
+ *         (at your option) any later version.<br>
+ *         <br>
+ *         This program is distributed in the hope that it will be useful,<br>
+ *         but WITHOUT ANY WARRANTY; without even the implied warranty of<br>
+ *         MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br>
+ *         GNU General Public License for more details.<br>
+ *         <br>
+ *         You should have received a copy of the GNU General Public License<br>
+ *         along with this program.  If not, see <https://www.gnu.org/licenses/>.<br>
+ */
+
 package holiday_mod.data;
 
 import holiday_mod.loot.AddItemModifier;
